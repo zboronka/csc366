dfd90e9798be2bae61cd84020afa2baeb69aec15
Zachary Kurt Boronka <zboronka@gmail.com>
2019-10-21 20:05:40 -0400

    Crypto xps wark

--- crypto_rpg/crypto.pl
+++ crypto_rpg/crypto.pl
@@ -18,10 +18,10 @@ generate_random_crypto_problem :-
 	generate_random_number(N4),
 	generate_random_number(N5),
 	generate_random_number(G),
-	declare(crypto_problem, (problem(N1,N2,N3,N4,N5),goal(G))),!.
+	declare(crypto_problem, problem(numbers(N1,N2,N3,N4,N5),goal(G))).
 
 display_problem :-
-	value_of(crypto_problem, (problem(N1,N2,N3,N4,N5),goal(G))),
+	value_of(crypto_problem, problem(numbers(N1,N2,N3,N4,N5),goal(G))),
 	write('numbers = {'),write(N1),write(','),write(N2),write(','),write(N3),write(','),write(N4),write(','),write(N5),write('} and goal = '),write(G),nl.
 
 display_problems(1) :-
@@ -31,3 +31,8 @@ display_problems(N) :-
 	display_problems(1),
 	NM1 is N - 1,
 	display_problems(NM1).
+
+add_crypto_problem_to_KB(_,_,_,_,_,_) :-
+	undeclare(crypto_problem), fail.
+add_crypto_problem_to_KB(N1,N2,N3,N4,N5,G) :-
+	declare(crypto_problem, problem(numbers(N1,N2,N3,N4,N5),goal(G))).
